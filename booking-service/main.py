598ca82dadc9e03c1be76e761f33ed1105dcfc27
FilipProjcevski <f.projcevski@gmail.com>
2024-09-10 21:42:48 +0200

    Modified a little bit in the booking-service.

--- booking-service/main.py
+++ booking-service/main.py
@@ -12,12 +12,8 @@ async def book_tickets(movie_id: int, quantity: int):
     response = requests.put(f"{INVENTORY_SERVICE_URL}/{movie_id}/book", params={"tickets": quantity})
     if response.status_code == 200:
         return {"message": "Tickets successfully booked", "movie": response.json()}
-    elif response.status_code == 400:
-        raise HTTPException(status_code=400, detail=response.json())
-    elif response.status_code == 404:
-        raise HTTPException(status_code=404, detail=response.json())
     else:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
+        raise HTTPException(status_code=400, detail=response.json())
 
 
 @app.get("/")
